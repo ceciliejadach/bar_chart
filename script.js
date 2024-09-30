feae613705e6126e4ab67b6cd729818d60d15d05
Cecilie <cepe0001@stud.kea.dk>
2024-09-30 15:25:15 +0200

    final final commit

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// const list = document.querySelector("ul");
-// const valueArr = [];
-
-// setInterval(generateBars, 1000);
-// function generateBars() {
-//   const randomNum = Math.floor(Math.random() * 100);
-
-//   if (valueArr.length > 9) {
-//     valueArr.shift();
-//   }
-
-//   valueArr.push(randomNum);
-
-//   console.log(valueArr);
-//   const li = document.createElement("li");
-//   li.style.setProperty("--height", randomNum);
-//   list.appendChild(li);
-// }
-
 const list = document.querySelector("ul");
 const valueArr = [];
 
